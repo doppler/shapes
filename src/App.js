a2b3c6d9b38ee329517c1e132ffbdc62e49d5b15
David Rose <doppler@gmail.com>
2019-02-10 21:15:48 -0500

    this is pretty neat

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  const [count, setCount] = useState(5);
+  const [hPercent, sethPercent] = useState(34);
+  const [vPercent, setvPercent] = useState(50);
+  const handleCountChange = event => setCount(Number(event.target.value));
+  const handleHchange = event => sethPercent(Number(event.target.value));
+  const handleVchange = event => setvPercent(Number(event.target.value));
+  return (
+    <div className="App">
+      <CountControl {...{ count, handleCountChange }} />
+      <Hcontrol {...{ hPercent, handleHchange }} />
+      <Vcontrol {...{ vPercent, handleVchange }} />
+      <div className="container">
+        {Array.from(Array(count)).map((_, i) => (
+          <Square
+            key={i}
+            angle={(360 / count) * i}
+            {...{ hPercent, vPercent }}
+          />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
+
+const Square = ({ angle, vPercent, hPercent }) => {
+  console.log(
+    `polygon(50% 0, ${hPercent}% ${vPercent}%, ${100 - hPercent}% ${vPercent}%)`
+  );
+  return (
+    <div
+      className="Square"
+      style={{
+        transform: `rotate(${angle}deg)`,
+        WebkitClipPath: `polygon(50% 0, ${hPercent}% ${vPercent}%, ${100 -
+          hPercent}% ${vPercent}%)`,
+        background: `hsla(${angle}, 100%, 50%, 0.5)`
+      }}
+    />
+  );
+};
+const CountControl = ({ count, handleCountChange }) => {
+  return (
+    <input
+      type="range"
+      value={count}
+      min={1}
+      max={90}
+      step={1}
+      onChange={handleCountChange}
+    />
+  );
+};
+const Hcontrol = ({ hPercent, handleHchange }) => (
+  <input
+    type="range"
+    value={hPercent}
+    min={0}
+    max={100}
+    step={1}
+    onChange={handleHchange}
+  />
+);
+const Vcontrol = ({ vPercent, handleVchange }) => (
+  <input
+    type="range"
+    value={vPercent}
+    min={0}
+    max={100}
+    step={1}
+    onChange={handleVchange}
+  />
+);
