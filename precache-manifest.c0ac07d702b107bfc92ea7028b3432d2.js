1960602aceb6d0fd935f06a1cab82c0c7f7503aa
David Rose <doppler@gmail.com>
2019-02-10 22:24:10 -0500

    Updates

---
+++ precache-manifest.c0ac07d702b107bfc92ea7028b3432d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07b813b7896b345dc408",
+    "url": "/shapes/static/css/main.650c13ec.chunk.css"
+  },
+  {
+    "revision": "07b813b7896b345dc408",
+    "url": "/shapes/static/js/main.07b813b7.chunk.js"
+  },
+  {
+    "revision": "e570d835b4f221ef9992",
+    "url": "/shapes/static/js/runtime~main.e570d835.js"
+  },
+  {
+    "revision": "1cf9c136764d35e6da0a",
+    "url": "/shapes/static/js/2.1cf9c136.chunk.js"
+  },
+  {
+    "revision": "90202b1c9dbc6f3bf12d25dc23a0f83d",
+    "url": "/shapes/index.html"
+  }
+];
