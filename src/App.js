4d2eff2b12249669f20bbf46e6e43d8687e90200
David Rose <doppler@gmail.com>
2019-02-11 00:14:59 -0500

    move search destroyer to index.js

--- src/App.js
+++ src/App.js
@@ -65,9 +65,7 @@ const App = () => {
     if (match) dispatch({ type: "hPercent", hPercent: Number(match[1]) });
     match = hash.match(/vPercent=(\d+)/);
     if (match) dispatch({ type: "vPercent", vPercent: Number(match[1]) });
-    // get rid of stupid facebook tracking param -- thanks Karl Tiedt!
-    window.location.search = "";
-  });
+  }, []);
 
   // In this Effect, we're setting the URL hash (the part after the #) with
   // the values from the state variables.
